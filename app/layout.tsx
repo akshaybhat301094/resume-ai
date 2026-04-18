1acb88cd435dfbfe8f151626d0466ffbe2969706
akshaybhat301094 <akshayb@Akshays-MacBook-Air.local>
2026-04-18 20:54:06 +0530

    enabled analytics and speedinsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk, Space_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const inter = Inter({
@@ -43,6 +45,8 @@ export default function RootLayout({
     >
       <body className="h-[100dvh] overflow-hidden flex flex-col bg-[#F9F9F9]">
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
